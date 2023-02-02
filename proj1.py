3dfab039a975d5c6b971ef81489a230b9ebadd93
Arman Rahim <armanrahim2024@u.northwestern.edu>
2023-02-01 20:40:57 -0600

    added get_host func

--- proj1.py
+++ proj1.py
@@ -1,34 +1,66 @@
 import re
 import json
-def get_winners():
-    awards = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
+import nltk
+import spacy
+nlp = spacy.load('en_core_web_lg')
+
+def load_data():
     data = open('gg2013.json')
     tweets = json.load(data)
+    return tweets
+
+
+def get_winners(tweets):
+    banned_words = ['golden', 'globes', 'rt', 'goldenglobes']
     res = []
     dict = {}
     for tweet in tweets:
         txt = tweet['text']
         txt = txt.lower()
         txt = re.sub(r'[^\w\s]', '', txt)
-        win = re.search(r'(.*) (wins|won) (.*)', txt)
+        win = re.search(r'(.*) (wins|won) best (.*)', txt)
         if (win != None):
             str = win.group(1)
             split_str = str.split()
             str2 = win.group(3)
-            for a in awards:
-                a = a.lower()
-                a = re.sub(r'[^\w\s]', '', a)
-                award = re.search(a, str2)
-                if (award):
-                    award_str = award.group()
-                    res.append((' '.join(split_str[-2:]), award_str))
+            split_str2 = str2.split()
+            prev = split_str2[0]
+            if prev not in banned_words:
+                res.append(prev)
+            for i in range(1, len(split_str2)):
+                if((prev not in banned_words) and (split_str2[i] not in banned_words)):
+                    res.append(prev + ' ' + split_str2[i])
+                    prev = prev + ' ' + split_str2[i]
     win_set = set(res)
-    for winner in win_set:
-        dict[winner] = res.count(winner)
-    dict = sorted(dict.items(), key = lambda x: x[1])
-    #winners = list(filter(lambda x: x[1] >= 10, dict))
-    return dict
+    return sorted(win_set, key = res.count)
 
-print(get_winners())
+def get_host(tweets):
+    possible_candidates = []
+    for tweet in tweets:
+        #clean tweet
+        txt = tweet['text']
+        txt = txt.lower()
+        txt = re.sub(r'[^\w\s]', '', txt)
+        #regex to get part of sentence before the words host/hosts/hosted
+        win = re.search(r'(.*) host[s|ed]?', txt)
+        #if there is a match, separate the part that becomes before the 'host' word
+        if (win != None):
+            str = win.group(1)
+            #skip rt phrases
+            if("rt" in str):
+                continue
+            #use spacy named-entity recognition
+            doc = nlp(str)
+            for i in doc.ents:
+                if (i.label_ == 'PERSON'):
+                    possible_candidates.append(i.text)
+    possible_set = set(possible_candidates)
+    final_candidates = []
+    for c in possible_set:
+        if((possible_candidates.count(c)/len(possible_candidates)) >= 0.15):
+            final_candidates.append(c)
+    return final_candidates
+data = load_data()
+print(get_winners(data))
 
 
