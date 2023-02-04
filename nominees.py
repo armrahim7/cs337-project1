66747630e8a86019aeb41d9ffee0fce258f54251
Arman Rahim <armanrahim2024@u.northwestern.edu>
2023-02-03 19:50:18 -0600

    get_nominees function uses get_awards output

--- nominees.py
+++ nominees.py
@@ -3,21 +3,19 @@ import json
 import nltk
 import spacy
 nlp = spacy.load('en_core_web_lg')
+from awards import get_awards
 
 def load_data():
     data = open('gg2013.json')
     tweets = json.load(data)
     return tweets
-def get_nominees(tweets):
-    candidates = dict()
-    people_words = ['actor', 'actress']
-    awards = [['supporting', 'motion'],['motion', 'drama'], ['motion', 'musical', 'comedy'],['television', 'drama'],
-    ['television', 'musical', 'comedy'], ['television', 'miniseries'], ['supporting', 'television']]
+def get_nominees(awards, tweets):
     award_cands = dict()
+    award_names = []
+    for a in awards:
+        award_names.append(a.split())
+        award_cands['best ' + ' '.join(a.split())] = []
     retweet_combinations = [' rt ', 'rt ', ' rt']
-    for p in people_words:
-        for a in awards:
-            award_cands[p + ' ' + ' '.join(a)] = []
     for tweet in tweets:
         txt = tweet['text']
         txt = txt.lower()
@@ -28,20 +26,18 @@ def get_nominees(tweets):
         txt = re.sub(r'[G|g]olden [G|g]lobes', '', txt)
         if (txt.startswith('rt')):
             continue
-        for a in awards:
+        for a in award_names:
             if (all([x in txt for x in a])):
-                for p in people_words:
-                    if p in txt:
-                        doc = nlp(txt)
-                        for i in doc.ents:
-                            if(i.text.startswith('http') or any([x in i.text for x in retweet_combinations])):
-                                continue
-                            if (i.label_ == 'PERSON'):
-                                award_cands[p + ' ' + ' '.join(a)].append(i.text)
+                    doc = nlp(txt)
+                    for i in doc.ents:
+                        if(i.text.startswith('http') or any([x in i.text for x in retweet_combinations])):
+                            continue
+                        if (i.label_ == 'PERSON'):
+                            award_cands['best ' + ' '.join(a)].append(i.text)
     final_award_cands = dict()
     for award in award_cands:
         final_award_cands[award] = max(set(award_cands[award]), key = award_cands[award].count)
     return final_award_cands
-
 data = load_data()
-print(get_nominees(data))
+awards = get_awards(data)
+print(get_nominees(awards, data))
