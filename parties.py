ca61cd5b04134b726d4ab8b6c725fa38c3cfe369
Dwayne Morgan <93851336+1Iconic1@users.noreply.github.com>
2023-02-05 20:56:18 -0600

    Update parties.py

--- parties.py
+++ parties.py
@@ -14,28 +14,29 @@ z = open('gg2013.json')
 tweets = json.load(z)
 NER = spacy.load("en_core_web_sm")
 
-x=0
-parties={}
 
 
-def find_parties(twt):
-    party_re= re.search(r'(.*)(party|parties)(.*)',twt.lower())
-    if party_re != None: 
-        ppl=NER(twt)
-        pos_rating=sia.polarity_scores(twt)['compound']
-        for i in ppl.ents:
-            if i.label_ == 'PERSON' or i.label_ == 'ORG':
-                if 'Globes' not in i.text :
-                    if i.text not in parties:
-                        parties[i.text]=[1,pos_rating]
-                    else:
-                        parties[i.text][0]+= 1
-                        parties[i.text][1] = (parties[i.text][1] +pos_rating)/parties[i.text][0]
-
-for tweet in tweets:
-    find_parties(tweet['text'])     
+def find_parties(twts):
+    parties={}
+    for twt in twts:
+        party_re= re.search(r'(.*)(party|parties)(.*)',twt.lower())
+        if party_re != None: 
+            ppl=NER(twt)
+            pos_rating=sia.polarity_scores(twt)['compound']
+            for i in ppl.ents:
+                if i.label_ == 'PERSON' or i.label_ == 'ORG':
+                    if 'Globes' not in i.text :
+                        if i.text not in parties:
+                            parties[i.text]=[1,pos_rating]
+                        else:
+                            parties[i.text][0]+= 1
+                            parties[i.text][1] = (parties[i.text][1] +pos_rating)/parties[i.text][0]
+     return parties
+                                                                                                
+   
 
 def best_party():
+    find_parties()
     best=max(parties, key=parties.get)
 
     if parties[best][1]> 0:
@@ -43,9 +44,9 @@ def best_party():
     else:
         sentiment= 'hated it!'
 
-    print((f'Most Talked About party was thrown by {best} and people {sentiment}'))
+    return f'The most talked about party was thrown by {best} and people {sentiment}'
 
-best_party()
+#best_party()
 
 
 
